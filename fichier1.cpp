71ef3d67cd3393f9d97834b6443bd9dd1abba6dc
lovelinegatsi <lovelinegatsi@gmail.com>
2026-09-16 20:25:12 +0100

    modif

--- fichier1.cpp
+++ fichier1.cpp
@@ -1,1 +1,17 @@
 //creation de mon premier fichier
+
+#include <iostream>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+    int e = 50;
+    int f = 60;
+
+    std::cout << a + b << std::endl;
+
+    return 0;
+}
