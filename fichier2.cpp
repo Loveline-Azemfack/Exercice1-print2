c204f9a2da8ffbf39d4cc71cacb412efb8dc327b
lovelinegatsi <lovelinegatsi@gmail.com>
2026-09-17 18:33:35 +0100

    anonce de la somme de 2 +6

--- fichier2.cpp
+++ fichier2.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout <<"bonjour a tous !"<< std::endl;
     std::cout<< "je viens de faire 2 +4" << std::endl;
 
+        std::cout<<"ajout d'une modification pour pousser apres";
     return 0;
 }
